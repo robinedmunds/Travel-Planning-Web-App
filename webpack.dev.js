c6fbd23104c086486037b3dfce863be9145464fd
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-01 00:57:29 +0000

    Add styles rules to webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,6 +13,21 @@ module.exports = {
     compress: true,
     port: 8080
   },
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ],
+      }
+    ]
+  },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/client/views/ui.html",
