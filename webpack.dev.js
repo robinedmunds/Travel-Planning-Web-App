90ef5280d4439881cb4e690796a64543b6ef2e36
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-01 00:26:59 +0000

    Configure html-webpack-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -6,5 +7,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/ui.html",
+      filename: "./index.html"
+    })
+  ]
 };
