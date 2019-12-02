7b34a26197074e5fac7b9190ceb1a378d59d9fa4
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-02 01:45:56 +0000

    Create basic desktop design. Add multiple styles files

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,9 @@
 // js
-import "./js/main.js";
+// import "./js/main.js";
 
 // styles
 import "./styles/reset.css";
-import "./styles/base.css";
+import "./styles/base.scss";
+import "./styles/layout.scss";
+import "./styles/travel-card.scss";
+import "./styles/trip-form.scss";
