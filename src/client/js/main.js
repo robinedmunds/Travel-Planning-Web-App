e626079f3daf7921e8d678dbeb9f2988fffa1985
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-03 02:11:09 +0000

    Add DOM error feedback on invalid new trip submit

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -9,29 +9,33 @@ function addTripButtonClickCallback(event) {
   const destinationInput = document.getElementById("destination");
   const destination = destinationInput.value;
   if (!destination) {
-    console.log("No dest entered");
-    // TODO: add error feedback to DOM
+    console.log("No dest entered");  // TODO: remove, testing
+    destinationInput.classList.add("borders-danger");
+  } else {
+    destinationInput.classList.remove("borders-danger");
   };
 
   const departureInput = document.getElementById("departure");
   const departure = departureInput.value;
-  if (departure) {
+  if (!departure) {
+    departureInput.classList.add("borders-danger");
+  } else {
     const departureArray = departure.split("/").reverse();
     const isoDate = departureArray.join("-");
     if (!isNaN(Date.parse(isoDate))) {
+      departureInput.classList.remove("borders-danger");
       const dateObj = new Date(isoDate);
-      console.log(dateObj);
+      console.log(dateObj);  // TODO: remove, testing
     } else {
-      console.log("date invalid");
-      // TODO: add error feedback to DOM
+      console.log("date invalid");  // TODO: remove, testing
+      departureInput.classList.add("borders-danger");
     };
   };
-
   // TODO: if both inputs valid, make fetch req to backend
 };
 
 function main() {
-  addDummyTravelCards()
+  addDummyTravelCards();
   const addTripButton = document.getElementById("add-trip");
   addTripButton.addEventListener("click", (event) => { addTripButtonClickCallback(event) });
 };
