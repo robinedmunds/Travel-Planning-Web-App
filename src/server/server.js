87f9e3c6a69668719f8fce8ac0a1aff88e5b4506
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-03 19:52:16 +0000

    Add function buildTravelCardResponseDummy in server.js

--- src/server/server.js
+++ src/server/server.js
@@ -18,6 +18,20 @@ app.listen(LISTEN_PORT, () => {
   console.log(`Server started on port: ${LISTEN_PORT}`);
 });
 
+function buildTravelCardResponseDummy(destination, departDate) {
+  return {
+    city: "Pietermaritzburg",
+    country: "South Africa",
+    departDate: new Date(2020, 4 - 1, 24),
+    picture: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Upland_South_Africa_Savanna.jpg",
+    weather: {
+      high: 32,
+      low: 24,
+      forecast: "Hot and sunny throughout the day. Wear a hat."
+    }
+  };
+};
+
 app.get("/", (req, res) => {
   res.send("responding on path \"/\"");
 });
@@ -28,11 +42,8 @@ app.post("/api/travel-card", (req, res) => {
   if (postData.destination && postData.departDate) {
     const destination = postData.destination;
     const departDate = new Date(postData.departDate);  // TODO: validate date
-
-    const bodyString = JSON.stringify(req.body);
-    console.log(bodyString);
-    const departDateUK = [departDate.getDate(), departDate.getMonth()+1, departDate.getFullYear()].join("/");
-    res.send(`destination: ${destination}\ndepartDate: ${departDateUK}`);
+    const response = buildTravelCardResponseDummy(destination, departDate);
+    res.send(JSON.stringify(response));
   } else {
     res.send("Error: Expected POST data invalid or missing. e.g destination=\"Paris, UK\"&departDate=\"2021-04-1T00:00:00.000Z\"");
   };
