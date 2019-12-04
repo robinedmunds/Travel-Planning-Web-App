8ee6711f77fa07828564f017ce00a0a74194e373
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-04 21:12:07 +0000

    Fix import and dates addDummy.js

--- src/client/js/addDummy.js
+++ src/client/js/addDummy.js
@@ -1,12 +1,12 @@
 "use strict";
 
-import { travelCard } from "./travelCard";
+import { travelCardHTML } from "./travelCardHTML";
 
 export function addDummyTravelCards() {
   const dummyRes = {
     city: "Pietermaritzburg",
     country: "South Africa",
-    departDate: new Date(2020, 4 - 1, 24),
+    departDate: "2020-4-24",
     picture: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Upland_South_Africa_Savanna.jpg",
     weather: {
       high: 32,
@@ -18,7 +18,7 @@ export function addDummyTravelCards() {
   const dummyRes2 = {
     city: "London",
     country: "UK",
-    departDate: new Date(2021, 8 - 1, 13),
+    departDate: "2021-7-17",
     picture: "https://upload.wikimedia.org/wikipedia/commons/b/b3/City_hall_London_at_dawn_%28cropped%29.jpg",
     weather: {
       high: 19,
@@ -28,6 +28,6 @@ export function addDummyTravelCards() {
   };
 
   const main = document.querySelector("main");
-  main.insertAdjacentHTML("afterbegin", travelCard(dummyRes));
-  main.insertAdjacentHTML("afterbegin", travelCard(dummyRes2));
+  main.insertAdjacentHTML("afterbegin", travelCardHTML(dummyRes));
+  main.insertAdjacentHTML("afterbegin", travelCardHTML(dummyRes2));
 };
