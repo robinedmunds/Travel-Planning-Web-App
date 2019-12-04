2e42e1876eec9c275c6eab24ef6de72bf74a4f06
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-04 17:33:17 +0000

    Improve readability, server.js

--- src/server/server.js
+++ src/server/server.js
@@ -23,17 +23,19 @@ app.listen(LISTEN_PORT, () => {
 async function fetchAPI(url, method) {
   const response = await fetch(url, { method })
     .then(res => res);
+
   if (response.ok) {
     return await response.json();
   };
 };
 
 async function getDestinationCoords(destination) {
-  const credentials = JSON.parse(process.env.GEONAMES);
-  const url = `http://api.geonames.org/searchJSON?q=${destination}&maxRows=1&username=${credentials.username}&password=${credentials.password}`;
-  const method = "GET";
   try {
+    const credentials = JSON.parse(process.env.GEONAMES);
+    const url = `http://api.geonames.org/searchJSON?q=${destination}&maxRows=1&username=${credentials.username}&password=${credentials.password}`;
+    const method = "GET";
     const response = await fetchAPI(url, method);
+
     if (response.totalResultsCount > 0) {
       const latitude = response.geonames[0].lat;
       const longitude = response.geonames[0].lng;
@@ -41,46 +43,53 @@ async function getDestinationCoords(destination) {
       const country = response.geonames[0].countryName;
       return { coords: {latitude, longitude}, city, country };
     };
+
   } catch (err) {
     console.log("Error in getDestinationCoords: -\n" + err);
   };
 };
 
 async function getWeatherData(coords, date=null) {
-  const key = process.env.DARKSKY_KEY;
-  const latitude = coords.latitude;
-  const longitude = coords.longitude;
-  let url = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}?exclude=currently,flags`;
-  const method = "GET";
-  if (date) {
-    const epochTime = Math.floor(date / 1000);
-    url = url + `,${epochTime}`;
-  };
   try {
-    const toCelsius = (fahrenheit) => Math.round((fahrenheit-32) * (5/9));
+    const key = process.env.DARKSKY_KEY;
+    const latitude = coords.latitude;
+    const longitude = coords.longitude;
+    let url = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}?exclude=currently,flags`;
+    const method = "GET";
+
+    if (date) {
+      const epochTime = Math.floor(date / 1000);
+      url = url + `,${epochTime}`;
+    };
+
     const response = await fetchAPI(url, method);
+
     if (response) {
+      const toCelsius = (fahrenheit) => Math.round((fahrenheit-32) * (5/9));
       const high = toCelsius(response.daily.data[0].temperatureHigh);
       const low = toCelsius(response.daily.data[0].temperatureLow);
       const forecast = response.daily.data[0].summary;
       return { high, low, forecast };
     };
+
   } catch (err) {
     console.log("Error in getWeatherData: -\n" + err);
   };
 };
 
 async function getDestinationPicture(query) {
-  const key = process.env.PIXABAY_KEY;
-  const url = `https://pixabay.com/api/?q=${query}&image_type=photo&min_height=720&orientation=horizontal&safesearch=true&category=travel&key=${key}`;
-  const method = "GET";
   try {
+    const key = process.env.PIXABAY_KEY;
+    const url = `https://pixabay.com/api/?q=${query}&image_type=photo&min_height=720&orientation=horizontal&safesearch=true&category=travel&key=${key}`;
+    const method = "GET";
     const response = await fetchAPI(url, method);
+
     if (response.totalHits > 0) {
       const picture = response.hits[0].largeImageURL;
       const tags = response.hits[0].tags;
       return { picture, tags };
     };
+
   } catch (err) {
     console.log("Error in getDestinationPicture: -\n" + err);
   };
@@ -99,14 +108,17 @@ async function buildTravelCardResponse(destination, departDate) {
 
   try {
     const geonames = await getDestinationCoords(destination);
+
     if (geonames) {
       const darksky = await getWeatherData(geonames.coords, departDate);
       const pixabay = await getDestinationPicture(destination);
+
       if (darksky && pixabay) {
         const instance = new TravelCardResponse(departDate, geonames, darksky, pixabay);
         return JSON.stringify(instance);
       };
     };
+
   } catch (err) {
     console.log("Error in buildTravelCardResponse: -\n" + err);
   }
@@ -123,6 +135,7 @@ app.post("/api/travel-card", async (req, res) => {
     const departDateJSON = req.body.departDate;
   
     if (destination && departDateJSON) {
+
       if (isValidDate(departDateJSON)) {
         const departDate = new Date(departDateJSON);
         const travelCardJSON = await buildTravelCardResponse(destination, departDate);
@@ -140,6 +153,7 @@ app.post("/api/travel-card", async (req, res) => {
     } else {
       res.status(400).send("Error: Expected POST data invalid or missing. e.g destination=\"London, UK\"&departDate=\"2021-04-1T00:00:00.000Z\"");
     };
+
   } catch (err) {
     console.log("Error in \"/api/travel-card\" callback: -\n" + err);
   };
