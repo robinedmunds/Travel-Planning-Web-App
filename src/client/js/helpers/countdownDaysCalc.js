3ce9c24ba7a6e5a65afe84c511fbb6e0ab58e6f4
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-05 00:25:30 +0000

    Remove redundant countdownDaysCalc.js

--- src/client/js/helpers/countdownDaysCalc.js
+++
@@ -1,7 +0,0 @@
-const countdownDaysCalc = (departDate) => {
-  const msInDay = 24 * 60 * 60 * 1000;
-  const difference = departDate - Date.now();
-  return Math.ceil(difference / msInDay);
-};
-
-export { countdownDaysCalc };
