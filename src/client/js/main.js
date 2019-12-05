323d8f64f3deb0dbd54bf6d66f8d17bc5688acc5
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-05 00:40:38 +0000

    Add addDummyTravelCards import to client main.js

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,12 +1,15 @@
 "use strict";
 
 import { addTripButtonClickCallback } from "./addTripBtnClick";
+import { addDummyTravelCards } from "./addDummy";  // TODO: remove testing
 
 function main() {
   const addTripButton = document.getElementById("add-trip");
   addTripButton.addEventListener("click", (event) => {
     addTripButtonClickCallback(event)
   });
+
+  // addDummyTravelCards();  // TODO: remove testing
 };
 
 main();
