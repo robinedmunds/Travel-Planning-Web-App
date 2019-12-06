a5ad27362687291e364051205ee4e7de9e8d0e54
Robin Edmunds <r.edmunds@live.co.uk>
2019-12-06 20:37:32 +0000

    Fix helpers/calcDayDiff.js not returning negative values

--- src/client/js/helpers/calcDayDiff.js
+++ src/client/js/helpers/calcDayDiff.js
@@ -3,11 +3,7 @@
 const calcDayDifference = (date1, date2) => {
   const msInDay = 24 * 60 * 60 * 1000;
   let difference;
-  if (date1 >= date2) {
-    difference = date1 - date2;
-  } else {
-    difference = date2 - date1;
-  };
+  difference = date1 - date2;
   return Math.ceil(difference / msInDay);
 };
 
